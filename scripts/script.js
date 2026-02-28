4bdd2632e50c9bf0fadb8d80690c32cc17b380f6
MarcoHelgeDev <yoshi-182@web.de>
2026-02-28 14:49:11 +0100

    add: next and prev function

--- scripts/script.js
+++ scripts/script.js
@@ -15,6 +15,8 @@ const myImg = [
 
 const myImgUrl = [];
 const dialogRef = document.querySelector(".js-dialog-gallery");
+let imgId = 0;
+
 
 const fillMyImgUrl = () => {
   let imgUrl = "./assets/img/gallery/";
@@ -29,17 +31,58 @@ const renderImgGallery = () => {
   for (let i = 0; i < myImgUrl.length; i++) {
     htmlContent += /*html*/ `
       <button class="js-img-btn btn-as-img">
-        <img onclick="openDialog()" src="${myImgUrl[i]}"/>
+        <img onclick="openDialog(${i})" src="${myImgUrl[i]}"/>
       </button>
     `;
   }
   myGalleryContent.innerHTML = htmlContent;
 };
 
+const renderDialogContent = () => {
 
-function openDialog() {
+}
+
+function openDialog(id) {
+  imgId = id;  
   dialogRef.showModal();
+  updateImgDialog(imgId)
+  updateDialogFooter(imgId)
+
   dialogRef.classList.add("opened");
+}
+
+const updateImgDialog = (id) => {
+  const imgDialog = document.querySelector('.js-dialog-section');
+  imgDialog.innerHTML = /*html*/ `
+         <img src="${myImgUrl[id]}"/>
+    `;
+}
+
+const updateDialogFooter = (id) => {
+   const footerDialog = document.querySelector('.js-dialog-footer');
+    footerDialog.innerHTML = /*html*/ `
+      <button class="dialog-btn dialog-btn-left" onclick="prevImg(${id-1})"></button>
+        <span>${id+1} / ${myImgUrl.length}</span>
+      <button class="dialog-btn dialog-btn-right" onclick="nextImg(${id+1})"></button>
+    `;
+}
+
+const nextImg = (id) => {
+  if(id < myImgUrl.length){
+    openDialog(id)
+  } else {
+    id = 0;
+    openDialog(id)
+  }
+}
+
+const prevImg = (id) => {
+  if(id >= 0){
+    openDialog(id)
+  } else {
+    id = myImgUrl.length;
+    openDialog(id-1)
+  }
 }
 
 function closeDialog() {
